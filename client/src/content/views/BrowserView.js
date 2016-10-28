915d73da807204eefff870d8f1315a9ac17b1915
Cengiz Krämer <crema@cremaAir.local>
2016-10-28 12:18:21 +0200

    modifiny gitignore

---
+++ client/src/content/views/BrowserView.js
@@ -0,0 +1,14 @@
+module.exports = Backbone.View.extend({
+  el: ".content",
+  template: _.template("<div class=''>Going to be our BrowserView</div>"),
+  initialize: function () {
+    this.render();
+  },
+  render: function () {
+    this.$el.html(this.template());
+    this.subRender();
+  },
+  subRender: function () {
+    
+  }
+});
